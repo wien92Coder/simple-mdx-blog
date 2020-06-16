06f4bb619376e672f56b033220c3628744c027e1
Wien Winarno <winarno92@gmail.com>
2020-06-16 07:28:41 +0700

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,5 +38,15 @@ module.exports = {
         ]
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+        ],
+        display: "swap",
+      },
+    },
   ],
 }
